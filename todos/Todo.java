29302123042886b77d1a7ec0ea7dfec8b7716e63
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-05-25 13:22:56 +0200

    add comments and readme

--- todos/Todo.java
+++ todos/Todo.java
@@ -1,8 +1,15 @@
 
-
+/**
+ * Todo represents one Todo Item.
+ * If you want to test your code,
+ * add getters for the fields and
+ * a constructor that initializes
+ * the fields.
+ */
 public class Todo
 {
     String title;
-    String description;
     boolean done;
+    String description;
+   
 }
--- todos/TodoList.java
+++ todos/TodoList.java
@@ -1,10 +1,29 @@
 
-
+import java.util.List;
+/**
+ * represents a TodoList.
+ * If you want to test the report and today
+ * methods,
+ * add a constructor and a method that initializes
+ * the TodoList with test data, and/or an add
+ * method.
+ */
 public class TodoList
 {
+    private List<Todo> todos;
     /**
-     * returns a comma sep. String with all done todos.
+     * returns a comma seperated
+     * String with all done todos.
      */
     public String report(){
+        return null;
+    }
+    
+     /**
+     * returns a comma seperated
+     * String with all done todos.
+     */
+    public String today(){
+        return null;
     }
 }
